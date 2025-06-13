af5cc3f9da19112c8979af6fe79265aa9e0bbf65
Yadu28 <ortegaduvan11@gmail.com>
2025-06-13 09:07:48 -0500

    js.duv

---
+++ js/aprender.js
@@ -0,0 +1,30 @@
+console.log("hola mundo")
+//variables = let,ver,const
+
+const edad = 18;
+let edad_dos = 18;
+
+edad_dos = edad_dos + 2;
+console.log(edad_dos)
+//var entero = 20
+
+var entero = 20;
+var decimal = 20.5;
+var string = "hola"
+console.long (string)
+var boolean = true
+console.log (boolean)
+var arrays_string = ["lun" , "mar" , "mierco" , "juev" , "viern" , "saba" , "domin"]
+console.log(arrays_string.string)
+
+console.log(arrays_string)
+console.log(arrays_string.length)
+var impresion = "";
+for (let i = 0; i < arrow.string.length; i++) {
+    console.log(arrays_string[i])
+    impresion = impresion + arrays_string[i]
+    Swal.fire("SweetAlert2 is working!");
+}
+Swal.fire(impresion);
+
+console.log()
