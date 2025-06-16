89940869159dc23e29ca96f2e32923629e7766c8
Yadu28 <ortegaduvan11@gmail.com>
2025-06-15 22:25:30 -0500

    actuali

--- js/aprender.js
+++ js/aprender.js
@@ -1,38 +1,34 @@
-console.log("hola mundo")
-//variables = let,ver,const
+console.log("hola profe")
+//variables = let,var,const
 
 const edad = 18;
 let edad_dos = 18;
+
 edad_dos = edad_dos + 2;
 console.log(edad_dos)
-//var entero = 20
-//var decimal =20.5
+
+//tipos de datos = entero decimal , string , arrays
+
 var entero = 20;
 var decimal = 20.5;
 var string = "hola"
-console.long (string)
-var boolean = true
-console.log (boolean)
-var arrays_string = ["lun" , "mar" , "mierco" , "juev" , "viern" , "saba" , "domin"]
-console.log(arrays_string)
-
-
-for (let i = 0; i < arrays_string.length; i++) {
-    console.log(arrays_string[i])
-    Swal.fire("SweetAlert2 is working!");
-}
+var boolean = true //false
+console.log(boolean)
+var arrays_string = ["lun", "Mar","Mier","Jue","Vier"];
 
 console.log(arrays_string)
-console.log(arrays_string.length)
-var impresion = "";
-for (let i = 0; i < arrow.string.length; i++) {
-    console.log(arrays_string[i])
-    impresion = impresion + arrays_string[i]
-}
-Swal.fire(impresion);
+//operadores aritmaticos +,-,*,/ %}
 
-var num1 = 14
-var num2 = 15 
-var suma = num1 + num2
-console.log(suma)
+//Operadores de comparacion 
 
+//Estructuras logicas
+//Inicializador 
+ for (let i = 0; i < arrays_string.length; i++) {
+  console.log(arrays_string[i])
+  Swal.fire({
+  icon: "error",
+  title: "Oops...",
+  text: "Something went wrong!",
+  footer: '<a href="#">Why do I have this issue?</a>'
+});
+ }
